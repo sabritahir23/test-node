811b1f33d7752f2d919d1affccdc5c936fee6ada
sabritahir23 <55855625+sabritahir23@users.noreply.github.com>
2020-05-20 11:42:42 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-var http = require("http"); 
-http.createServer(function(req,res) {
-  var path = req.url.toLowerCase();
-  switch(path) {
-    case '/':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('Home page');
-      break;
-    case '/about':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('About page');
-      break;
-    default:
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('Not found');
-      break;
+var fs = require('fs'),
+    http = require('http');
+
+http.createServer(function (req, res) {
+  fs.readFile(__dirname + req.url, function (err,data) {
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
     }
-}).listen(process.env.PORT || 3000);
+    res.writeHead(200);
+    res.end(data);
+  });
+}).listen(3000);
+
